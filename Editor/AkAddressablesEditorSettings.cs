a0e3af53edc7508fc3715666fdec92064b894bb9
svc-jenkins <svc-jenkins@hq.audiokinetic.com>
2025-01-01 16:39:18 -0500

    Jenkins: Updated Copyrights notice to current year.
    Change-Id: Ia2eeb89aabd468cc7c706f98c7569869a1e168a0

--- Editor/AkAddressablesEditorSettings.cs
+++ Editor/AkAddressablesEditorSettings.cs
@@ -12,7 +12,7 @@ Licensees holding valid licenses to the AUDIOKINETIC Wwise Technology may use
 this file in accordance with the end user license agreement provided with the
 software or, alternatively, in accordance with the terms contained
 in a written agreement between you and Audiokinetic Inc.
-Copyright (c) 2024 Audiokinetic Inc.
+Copyright (c) 2025 Audiokinetic Inc.
 *******************************************************************************/
 
 #if UNITY_EDITOR
