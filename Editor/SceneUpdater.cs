528998d01f01ad23ef65c20bf5f34a993de59c4f
Hugo Turgeon-Nozaki <htnozaki@audiokinetic.com>
2025-02-25 14:52:28 -0500

    WG-75862 Wwise Addressable : Create an Addressable Installer
    Change-Id: I39a3fe4a0f486bd84c3f8b9f8a6debc2c450192e
    (cherry picked from commit b6d6cdb5b0d207ec9052f03c7e2f2c61083e7e44)

--- Editor/SceneUpdater.cs
+++ Editor/SceneUpdater.cs
@@ -58,7 +58,14 @@ public class SceneUpdater
         {
             EditorApplication.hierarchyChanged -= OnHierarchyChanged;
             EditorApplication.update -= Update;
-            EditorUtility.DisplayDialog("Reload All Scenes", "All scenes have been reloaded.", "OK");
+            if (UnityEditorInternal.InternalEditorUtility.inBatchMode)
+            {
+                EditorApplication.Exit(0);
+            }
+            else
+            {
+                EditorUtility.DisplayDialog("Reload All Scenes", "All scenes have been reloaded.", "OK");
+            }
         }
     }
     
