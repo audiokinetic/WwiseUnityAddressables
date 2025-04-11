fa561432aaf4fd36ed6f1b290ad88cad788a407d
Hugo Turgeon-Nozaki <htnozaki@audiokinetic.com>
2025-04-11 16:31:49 -0400

    WG-78880 Wwise Unity Addressables: Ref Count is not reduced if bank handle is not found
    Change-Id: Icaf27ab42694c186742000bb31b1f839501c18ab
    (cherry picked from commit 8951d4fd45c6d6b550d2c7a7e6d2023137ba13cd)

--- Editor/SceneUpdater.cs
+++ Editor/SceneUpdater.cs
@@ -92,6 +92,10 @@ public class SceneUpdater
         {
             if (hierarchyChanged)
             {
+                if (UnityEditorInternal.InternalEditorUtility.inBatchMode)
+                {
+                    AkWwisePostImportCallbackSetup.CheckWwiseGlobalExistance();
+                }
                 Scene scene = SceneManager.GetActiveScene();
                 EditorSceneManager.SaveScene(scene);
             }
