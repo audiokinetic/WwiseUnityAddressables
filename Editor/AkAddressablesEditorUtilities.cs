301a62e5023e5c2041d93e58f0fa4d40e2b8c242
Hugo Turgeon-Nozaki <htnozaki@audiokinetic.com>
2025-08-14 14:52:37 -0400

    WG-78756 Unity: Error randomly appears after generating soundbanks | Adjust GetRootOutputPath
    Change-Id: Ie386a81cf643bbe0a1d0a0789078c903719e8fbc

--- Editor/AkAddressablesEditorUtilities.cs
+++ Editor/AkAddressablesEditorUtilities.cs
@@ -191,7 +191,7 @@ namespace AK.Wwise.Unity.WwiseAddressables
 				WwisePlatformRef platformInfo = new WwisePlatformRef(platformName);
 				if (platformInfo.Name == null)
 				{ 
-					WwiseProjectDatabase.Init(AkUtilities.GetRootOutputPath(AkWwiseEditorSettings.WwiseProjectAbsolutePath), platformName, language);
+					WwiseProjectDatabase.Init(AkWwiseEditorSettings.GetRootOutputPath(), platformName, language);
 				}
 			}
 			if (SoundbanksInfo.ContainsKey(platformName) && SoundbanksInfo[platformName].containsInvalidEntry)
@@ -211,7 +211,7 @@ namespace AK.Wwise.Unity.WwiseAddressables
 			WwiseSoundBankRef sbInfo = new WwiseSoundBankRef(newBankName, type);
 			if (!sbInfo.IsValid)
 			{
-				WwiseProjectDatabase.Init(AkUtilities.GetRootOutputPath(AkWwiseEditorSettings.WwiseProjectAbsolutePath), platformName, language);
+				WwiseProjectDatabase.Init(AkWwiseEditorSettings.GetRootOutputPath(), platformName, language);
 				sbInfo = new WwiseSoundBankRef(newBankName, type);
 			}
 			if (sbInfo.IsValid)
