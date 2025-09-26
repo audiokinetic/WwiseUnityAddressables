e29167e9ccec8c39a103e208ef30d4ed372793c4
Hugo Turgeon-Nozaki <htnozaki@audiokinetic.com>
2025-09-26 16:59:03 -0400

    WG-80296 The Wwise Unity Addressables is not backward compatible
    Change-Id: I87cbc4b3d17299368ab1c0d52a0d234a5b21ed95

---
+++ Runtime/WwiseAddressableAdapter/WwiseAddressableAdapterFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Reflection;
+public static class WwiseAddressableAdapterFactory
+{
+    public static IWwiseAddressableAdapter CreateManager()
+    {
+#if ADDRESSABLES_API_DEFAULT
+        return new WwiseAddressableAdapter_Default();
+#else
+        return new WwiseAddressableAdapter_Null();
+#endif
+    }
+}
