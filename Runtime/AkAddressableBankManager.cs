4cccfc0ef2e773978035a2f56d29091b0aa2a6da
Hugo Turgeon-Nozaki <htnozaki@audiokinetic.com>
2025-11-05 17:17:15 -0500

    WG-80920 Addressables: InitBankHolder errors appear when running the Installer
    Change-Id: I331f4f48749bc2d911a0b2902459b8d7325d962e

--- Runtime/AkAddressableBankManager.cs
+++ Runtime/AkAddressableBankManager.cs
@@ -289,6 +289,12 @@ namespace AK.Wwise.Unity.WwiseAddressables
 
 		private static WwiseAddressableSoundBank FindInitBank()
 		{
+			//Don't log an error from not finding the initBankHolder, as CreateWwiseGlobal will ensure the initBankHolder is added either on Scene creation or Scene reload from the reload script.
+			if (!Application.isPlaying && AkWwiseEditorSettings.Instance.CreateWwiseGlobal)
+			{
+				return null;
+			}
+
 			var foundBank = UnityEngine.MonoBehaviour.FindObjectsOfType<InitBankHolder>();
 			if (foundBank.Count() == 0)
 			{
@@ -403,7 +409,8 @@ namespace AK.Wwise.Unity.WwiseAddressables
 
 		public void UnloadInitBank()
 		{
-			if (InitBank != null)
+			//Not using the accessor here as if the initBank wasn't set on load, there's no point trying to unload it.
+			if (initBank != null)
 			{
 				BankHandle initBankHandle = new BankHandle(InitBank, ignoreRefCount: true, removeFromBankDictionary: false);
 				initBankHandle.UnloadBank();
