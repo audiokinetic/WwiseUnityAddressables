f935e8c7ccdc5e2086df52f653a425df8216fc82
Hugo Turgeon-Nozaki <htnozaki@audiokinetic.com>
2025-11-12 15:36:16 -0500

    WG-80752 Unity Addressables: Log spam when scrolling through Browser
    Change-Id: I46e067d1cdc6a7f5bfaf3588c371c84e9d1f4958

--- Runtime/WwiseAddressableSoundBank.cs
+++ Runtime/WwiseAddressableSoundBank.cs
@@ -33,6 +33,7 @@ namespace AK.Wwise.Unity.WwiseAddressables
 		[SerializeField]
 		internal WwiseBankPerPlatformEntry[] m_dataPerPlatformList;
 
+		[System.Obsolete("This field is obsolete.", true)]
 		[SerializeField]
 		internal WwiseBankPerPlatformEntry currentPlatformAssets;
 
@@ -70,6 +71,25 @@ namespace AK.Wwise.Unity.WwiseAddressables
 		public delegate string GetWwisePlatformNameDelegate(BuildTarget target);
 		public static GetWwisePlatformNameDelegate GetWwisePlatformNameFromBuildTarget;
 #endif
+
+		public WwiseBankPerPlatformEntry CurrentPlatformAssets
+		{
+			get
+			{
+				string wwisePlatform = GetWwisePlatformNameFromBuildTarget(EditorUserBuildSettings.activeBuildTarget);
+				if (m_dataPerPlatformList != null)
+				{
+					foreach (var entry in m_dataPerPlatformList)
+					{
+						if (entry.WwisePlatform == wwisePlatform)
+						{
+							return entry;
+						}
+					}
+				}
+				return null;
+			}
+		}
 
 		public uint SoundbankId
 		{
@@ -167,11 +187,6 @@ namespace AK.Wwise.Unity.WwiseAddressables
 					entry.LocalizedStreamingMediaKeys = entry.LocalizedStreamingMedia.Keys.ToArray();
 					entry.LocalizedStreamingMediaValues = new LocalizedStreamingMediaList();
 					entry.LocalizedStreamingMediaValues.Add(entry.LocalizedStreamingMedia.Values.ToList());
-
-					if (entry.WwisePlatform == wwisePlatform)
-					{
-						currentPlatformAssets = entry;
-					}
 				}
 			}
 #endif
